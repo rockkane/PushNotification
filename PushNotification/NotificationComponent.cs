874b566c62ce1d68efb0e7a6a00ad5f04a628f98
Your Name <you@example.com>
2018-07-23 17:42:38 +0600

    first commit

---
+++ PushNotification/NotificationComponent.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using Microsoft.AspNet.SignalR;
+
+namespace PushNotification
+{
+    public class NotificationComponent
+    {
+        public void RegisterNotification(DateTime currentTime)
+        {
+            string conStr = ConfigurationManager.ConnectionStrings["sqlConString"].ConnectionString;
+            string sqlCommand = @"Select [ContactID],[ContactName],[ContactNo] from [dbo].[Contacts] where [AddedOn]>@AddedOn";
+
+
+
+            using(SqlConnection con=new SqlConnection(conStr))
+            {
+                SqlCommand cmd = new SqlCommand(sqlCommand, con);
+                cmd.Parameters.AddWithValue("@AddedOn", currentTime);
+                if (con.State !=System.Data.ConnectionState.Open)
+                {
+                    con.Open();
+                }
+                cmd.Notification = null;
+                SqlDependency sqlDep = new SqlDependency(cmd);
+                sqlDep.OnChange += sqlDep_OnChange;
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+
+                }
+            }
+
+        }
+
+        void sqlDep_OnChange(object sender, SqlNotificationEventArgs e)
+        {
+            if (e.Type==SqlNotificationType.Change)
+            {
+                SqlDependency sqlDep = sender as SqlDependency;
+                sqlDep.OnChange -= sqlDep_OnChange;
+
+                var notificationHub = GlobalHost.ConnectionManager.GetHubContext<NotificationHub>();
+                notificationHub.Clients.All.notify("added");
+
+                RegisterNotification(DateTime.Now);
+            }
+        }
+
+
+        public List<Contact> GetContacts(DateTime afterDate)
+        {
+            using (MyPushNotificationEntities dc=new MyPushNotificationEntities())
+            {
+                return dc.Contacts.Where(a => a.AddedOn > afterDate).OrderByDescending(a => a.AddedOn).ToList();
+            }
+        }
+    }
+}
